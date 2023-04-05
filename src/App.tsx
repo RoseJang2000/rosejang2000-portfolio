a68b1da67d0cd6a5e098110eac475bbeb4a7a811
Rose Jang <jangmi749@gmail.com>
2023-04-05 13:48:11 +0900

    :sparkles: feat : Route 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import AboutMe from '@pages/AboutMe';
+import Home from '@pages/Home';
+import Projects from '@pages/Projects';
+import Skills from '@pages/Skills';
+import { Route, Routes } from 'react-router-dom';
+
 const App = () => {
-  return <div>APP</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutme" element={<AboutMe />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/skills" element={<Skills />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
