37246168d6ae45c7ea1ead2910680db116e73927
Rose Jang <jangmi749@gmail.com>
2023-04-05 17:21:17 +0900

    :lipstick: design : commandColor 추가

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -5,6 +5,7 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'Noto Sans KR', sans-serif;
   }
   a {
     text-decoration: none;
