083f74ba692cef8c6d9497bbd4a619ea55012c7e
Rose Jang <jangmi749@gmail.com>
2023-04-05 14:11:14 +0900

    :lipstick: design : light, dark mode 색상 선언

---
+++ src/styles/Theme.tsx
@@ -0,0 +1,28 @@
+const lightTheme = {
+  bgColor: '#fefefe',
+  titleColor: '#222',
+  descColor: '#484B6A',
+  accentColor: '#111',
+  boxColor: '#E4E5F1',
+  boxColor2: '#D2D3DB',
+  pointColor: '#3772FF',
+  lineColor: '#9394A5',
+};
+
+const darkTheme = {
+  bgColor: '#17202A',
+  titleColor: '#fff',
+  descColor: '#8899A6',
+  accentColor: '#fff',
+  boxColor: '#25303B',
+  boxColor2: '#1C2733',
+  pointColor: '#3772FF',
+  lineColor: '#8B98A5',
+};
+
+const theme = {
+  lightTheme,
+  darkTheme,
+};
+
+export default theme;
