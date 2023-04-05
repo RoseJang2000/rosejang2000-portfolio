74e792fcb12a4b49b54f85e728b1f8ea69e1824c
Rose Jang <jangmi749@gmail.com>
2023-04-05 16:19:39 +0900

    :lipstick: design : a 태그 기본 설정

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -8,11 +8,18 @@ export const GlobalStyle = createGlobalStyle`
   }
   a {
     text-decoration: none;
+    color: ${(props) => props.theme.colors.descColor};
+    transition: 0.2s;
+    :hover {
+      transition: 0.2s;
+      color: ${(props) => props.theme.colors.accentColor};
+    }
   }
   ul {
     list-style: none;
   }
   body {
-    background-color: ${(props) => props.theme.colors.bgColor}
+    background-color: ${(props) => props.theme.colors.bgColor};
+    color: ${(props) => props.theme.colors.descColor};
   }
 `;
