0c83cb236e7439bd02eeafe3c1abc6139bbc8ddf
Rose Jang <jangmi749@gmail.com>
2023-04-05 13:50:46 +0900

    :sparkles: feat : Global Style 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Home from '@pages/Home';
 import Projects from '@pages/Projects';
 import Skills from '@pages/Skills';
 import { Route, Routes } from 'react-router-dom';
+import { GlobalStyle } from './GlobalStyle';
 
 const App = () => {
   return (
     <>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/aboutme" element={<AboutMe />} />
