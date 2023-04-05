e2eb0629b9fdb0cda1b61b0eeb6aee47160a62b4
Rose Jang <jangmi749@gmail.com>
2023-04-05 22:45:23 +0900

    :sparkles: feat : project data 타입 설정

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,4 +1,30 @@
+import projects from '@assets/projects.json';
+
+export interface Project {
+  title: string;
+  desc: string;
+  skillDesc: string[];
+  team: null | {
+    front: number;
+    back: number;
+    myRole: string[];
+  };
+  links: Link[];
+  dev: {
+    start?: string;
+    end?: string;
+    period?: string;
+  };
+  stack: string[];
+}
+
+interface Link {
+  name: string;
+  url: string;
+}
+
 const Projects = () => {
+  const projectData: Project[] = projects;
   return <div>Projects</div>;
 };
 
